5a57647f7ce670b28d0dcfb6897d7775eb9d5a63
ranvir <ranvir26apr@gmail.com>
2024-03-20 16:18:09 +0530

    modified index.js & package.json

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-console.log("Hello from test/index.js!")
+function reverseInt(n) {
+    const reversed = n.toString().split('').reverse().join('');
+    return parseInt(reversed) * Math.sign(n);
+}
+console.log(reverseInt(-51))
